0a02be62501e1a03b73855585f3555669ae6cbc9
Kwan Hong Lee <kwan@redstar.com>
2014-01-10 06:36:46 -0500

    assets and content added for micro site --skip-ci

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass'
+gem "font-awesome-rails"
 
 ruby '2.0.0'
 
--- app/controllers/micro_controller.rb
+++ app/controllers/micro_controller.rb
@@ -2,14 +2,17 @@ class MicroController < ApplicationController
 
   def guidance 
     @page_title = "Guidance"
+    @user = User.new
   end
 
   def tracking
     @page_title = "Tracking"
+    @user = User.new
   end
 
   def social
     @page_title = "Social"
+    @user = User.new
   end
 
 end
