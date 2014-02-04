b1f805716c9e713e382b0318b6d2b558fed6dcd2
Kwan Hong Lee <kwan@redstar.com>
2014-02-04 11:23:56 -0500

    removed bitly since twitter reduces urls automagically --skip-ci

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -18,15 +18,8 @@ class ContentController < ApplicationController
   end
 
   def show 
-    client = Bitly.client
     c = Content.find_by_slug(params[:id])
-    @page_title = c.title
-    if Rails.env.development?
-      @short_url = request.original_url
-    else
-      long_url = request.original_url
-      @short_url = client.shorten(long_url).short_url
-    end
+    @content = c
 
     if request.get?
       # show particular article page
--- config/initializers/bitly.rb
+++
@@ -1,5 +0,0 @@
-Bitly.configure do |config|
-  config.api_version = 3
-  config.login = "o_5ja9njlaaj"
-  config.api_key = "R_59796415e9804019a3b6d5a380bbb10e"
-end
