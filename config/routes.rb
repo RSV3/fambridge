0a13acfb8e0ec46eddf4ea7a17756af4d5f43d56
Kwan Hong Lee <kwan@redstar.com>
2014-02-10 18:17:34 -0500

    changed root to point to content index --skip-ci

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Fambridge::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'micro#tools_landing'
+  root 'content#index'
 
   match '/start', to: 'main#index', via: 'get'
   match '/home', to: 'main#home', via: 'get'
